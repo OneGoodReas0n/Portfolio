5df1f30697f82d5c48a72057f4268034476828f9
Illia Korobov <korobovi4@gmail.com>
2020-05-14 23:27:49 +0200

    init: create structure containers for project

---
+++ src/containers/Home/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './home.scss';
+import Navbar from '../../components/Navbar';
+import Greetings from '../../components/Greetings';
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <Navbar />
+        <Greetings />
+      </div>
+    );
+  }
+}
+
+export default Home;
