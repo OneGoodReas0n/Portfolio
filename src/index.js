29f1e71d5b6b50c03813dbf5f99bf9c9010bb655
OneGoodReas0n <korobovi4@gmail.com>
2020-05-27 00:15:08 +0200

    update: import fonts from google.fonts api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import './styles/style.scss';
+import './styles/normalize.scss';
 import App from './containers/App';
 
 const wrapper = document.getElementById('main');
